d6f4e96218ab76f786e1dd522d094d1b68b92c6b
Ashymov Suultan <sashymov@gmail.com>
2020-03-28 18:40:17 +0600

    initial commit

---
+++ src/main/java/kg/megacom/auctionv2/Auctionv2Application.java
@@ -0,0 +1,13 @@
+package kg.megacom.auctionv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auctionv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auctionv2Application.class, args);
+	}
+
+}
